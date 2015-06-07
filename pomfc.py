e32c6eaabc1d41219c3c1437b73ceae942678d05
rikkamas <dontrunifyoucanwalk@gmail.com>
2015-06-08 00:16:39 +0100

    Added URL management
    Added a URL management that saves all URL's to "URLS.db" program checks
    if URL is already in database before uploading.

--- pomfc.py
+++ pomfc.py
@@ -1,6 +1,7 @@
 from sys import version
 from sys import argv
 from sys import exit
+
 import urllib2
 
 try:
@@ -24,21 +25,28 @@ def download():
 		fsave.write(dl.read())
 		fsave.close()	
 def upload():
-	try:
-		response = requests.post(
-			url="http://pomf.se/upload.php",
-			files={"files[]":open(file, "rb")}
-		)
-	except Exception as e:
-		print("Error uploading {0}".format(e))
-		exit()
+	with open("URLS.db", 'r') as inF:
+		for line in inF:
+			if file in line:
+				print "File already present in URL database"
+			else:
+				try:
+					response = requests.post(
+						url="http://pomf.se/upload.php",
+						files={"files[]":open(file, "rb")}
+					)
+				except Exception as e:
+					print("Error uploading {0}".format(e))
+					exit()
 
-	local = file.split('/')
-	local = (local[-1] if len(local) > 1 else local[0])
-
-	remote = "http://a.pomf.se/" + response.text.split('"')[17]
-
-	print(local + " -> " + remote)
+				local = file.split('/')
+				local = (local[-1] if len(local) > 1 else local[0])
+				remote = "http://a.pomf.se/" + response.text.split('"')[17]
+				print(local + " -> " + remote)
+				urltogo = file + " == " + remote
+				urlman = open("URLS.db", "a+")
+				urlman.write(urltogo + "\n")
+				urlman.close()
 def encrypt():
 	print("To Add")
 def decrypt():
@@ -50,10 +58,10 @@ def main():
 		upload()
 	elif(argv[1] == "-d"):
 		download()
-	elif(argv[1] == "-dd")
+	elif(argv[1] == "-dd"):
 		#download()
 		decrypt()
-	elif(argv[1] == "-eu")
+	elif(argv[1] == "-eu"):
 		encrypt()
 		#upload
 if __name__ == '__main__':
