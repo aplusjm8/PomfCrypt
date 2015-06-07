d0c42238f793b3057ad5560f602c73fe686968eb
rikkamas <dontrunifyoucanwalk@gmail.com>
2015-06-07 23:39:06 +0100

    Code reshuffle
    efficiency is key

--- pomfc.py
+++ pomfc.py
@@ -15,33 +15,46 @@ def usage():
 	print("Usage: " + argv[0] + " <filename>")
 	exit()
 
+file = argv[2]
+def download():
+		remote = "http://a.pomf.se/" + file
+		dl = urllib2.urlopen(remote)
+		print("Downloading " + remote + " and saving as " + file)
+		fsave = open(file, 'wb')
+		fsave.write(dl.read())
+		fsave.close()	
+def upload():
+	try:
+		response = requests.post(
+			url="http://pomf.se/upload.php",
+			files={"files[]":open(file, "rb")}
+		)
+	except Exception as e:
+		print("Error uploading {0}".format(e))
+		exit()
+
+	local = file.split('/')
+	local = (local[-1] if len(local) > 1 else local[0])
+
+	remote = "http://a.pomf.se/" + response.text.split('"')[17]
+
+	print(local + " -> " + remote)
+def encrypt():
+	print("To Add")
+def decrypt():
+	print("To Add")
 def main():
 	if len(argv) != 3:
 		usage()
-
-	file = argv[2]
 	if(argv[1] == "-u"):
-		try:
-			response = requests.post(
-				url="http://pomf.se/upload.php",
-				files={"files[]":open(file, "rb")}
-			)
-		except Exception as e:
-			print("Error uploading {0}".format(e))
-			exit()
-
-		local = file.split('/')
-		local = (local[-1] if len(local) > 1 else local[0])
-
-		remote = "http://a.pomf.se/" + response.text.split('"')[17]
-
-		print(local + " -> " + remote)
+		upload()
 	elif(argv[1] == "-d"):
-		remote = "http://a.pomf.se/" + file
-		print(remote)
-		dl = urllib2.urlopen(remote)
-		fsave = open(file, 'wb')
-		fsave.write(dl.read())
-		fsave.close()
+		download()
+	elif(argv[1] == "-dd")
+		#download()
+		decrypt()
+	elif(argv[1] == "-eu")
+		encrypt()
+		#upload
 if __name__ == '__main__':
 	main()
